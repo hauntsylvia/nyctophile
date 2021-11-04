b6f9bfeb2113f890fa9921f21a0f05596911cedd
Izabella Williams <iza.yy10@gmail.com>
2021-11-04 08:28:54 -0400

    respecting gitignore

--- out/server/static-endpoint-methods/currency.server.lua
+++
@@ -1,4 +0,0 @@
--- Compiled with roblox-ts v1.2.7
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
-local APIRegister = TS.import(script, game:GetService("ReplicatedStorage"), "TS", "modules", "api-register").APIRegister
-local thisService = APIRegister.new("currency")
